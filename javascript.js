266a84aba46bf6b664a6bfbe899a8d83bca45936
ajjdunn <109111846+ajjdunn@users.noreply.github.com>
2022-12-31 16:24:44 -0800

    Added functions to RPS

--- javascript.js
+++ javascript.js
@@ -1,15 +1,81 @@
-console.log("Hello");
-
-function getComputerChoice(min, max) {
-  let result = Math.floor(Math.random() * (max - min + 1) + min);
-  if (result == 1) {
-    return "Rock";
-  } else if (result == 2) {
-    return "Paper";
-  } else if (result == 3) {
-    return "Scissors";
-  }
-  return result;
+// Prompt user for a selection and return the value for later use
+function playerSelection() {
+  const playerChoice = prompt(
+    "Do you choose Rock, Paper, or Scissors"
+  ).toLocaleLowerCase();
+  console.log("Player chooses:", playerChoice);
+  return playerChoice;
 }
+// playerSelection();
 
-console.log(getComputerChoice(1, 3));
+// This function returns one of three choices at random
+function compSelection() {
+  let compChoice = Math.random();
+  if (compChoice < 0.34) {
+    compChoice = "rock";
+  } else if (compChoice <= 0.67) {
+    compChoice = "paper";
+  } else {
+    compChoice = "scissors";
+  }
+  console.log("Computer chooses:", compChoice);
+  return compChoice;
+}
+// compSelection();
+
+//this function compares player input and random computer selection and returns a string
+function playRound(playerChoice, compChoice) {
+  if (playerChoice == "rock" && compChoice == "scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerChoice == "rock" && compChoice == "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerChoice == "rock" && compChoice == "rock") {
+    return "Tie game!";
+  } else if (playerChoice == "scissors" && compChoice == "rock") {
+    return "You lose! Rock beats Scissors";
+  } else if (playerChoice == "scissors" && compChoice == "paper") {
+    return "You win! Scissors beats Paper";
+  } else if (playerChoice == "scissors" && compChoice == "scissors") {
+    return "Tie game!";
+  } else if (playerChoice == "paper" && compChoice == "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerChoice == "paper" && compChoice == "scissors") {
+    return "You lose! Scissors beats Paper";
+  } else if (playerChoice == "paper" && compChoice == "paper") {
+    return "Tie game!";
+  }
+}
+console.log(playRound(playerSelection(), compSelection()));
+
+//This function plays 5 round of the game and returns the score as a string
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let roundResult = playRound(playerChoice, compChoice);
+
+  for (let counter = 0; counter <= 5; ++counter) {
+    playRound(playerChoice, compChoice);
+
+    if (
+      roundResult === "You win! Rock beats Scissors" ||
+      roundResult === "You win! Scissors beats Paper" ||
+      roundResult === "You win! Paper beats Rock"
+    ) {
+      ++playerScore;
+    } else if (
+      roundResult === "You lose! Rock beats Scissors" ||
+      roundResult === "You lose! Scissors beats Paper" ||
+      roundResult === "You lose! Paper beats Rock"
+    ) {
+      ++computerScore;
+    }
+  }
+  if (playerScore > computerScore) {
+    return "You win" + playerScore + " to " + computerScore + ".";
+  } else if (playerScore < computerScore) {
+    return "You lose" + computerScore + " to " + playerScore + ".";
+  } else {
+    return "It was a tie, you both won the same number of rounds.";
+  }
+}
+game();
