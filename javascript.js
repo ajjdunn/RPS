f377cc89cd5356290e5bb9065effe425479e3a76
ajjdunn <aaronjjdunn@gmail.com>
2023-03-20 11:57:29 -0700

    Complete

--- javascript.js
+++ javascript.js
@@ -1,16 +1,38 @@
 let playerScore = 0;
 let computerScore = 0;
-let playerChoice = "paper";
-let compChoice = "scissors";
+let playerChoice = "";
+let compChoice = "";
+let playerChose = document.getElementById("player-chose");
+let computerChose = document.getElementById("computer-chose");
+const buttons = document.querySelectorAll(".button");
+let playAgain = document.querySelector(".play-again");
+let weapons = document.querySelector(".choice-container");
 
-// Prompt the player for an input
-function playerSelection() {
-  let playerChoice = prompt(
-    "Do you choose Rock, Paper, or Scissors"
-  ).toLocaleLowerCase();
-  console.log("Player chooses:", playerChoice);
-  return playerChoice;
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("I clicked " + button.id);
+    playerChoice = button.id;
+    playerChose.textContent = String("Player chose: " + button.id);
+    playRound();
+  });
+});
+
+playAgain.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  weapons.classList.remove("hidden");
+  playAgain.classList.add("hidden");
+  document.querySelector("#result").textContent = "Result: ";
+});
+
+// // Prompt the player for an input
+// function playerSelection() {
+//   let playerChoice = prompt(
+//     "Do you choose Rock, Paper, or Scissors"
+//   ).toLocaleLowerCase();
+//   console.log("Player chooses:", playerChoice);
+//   return playerChoice;
+// }
 
 // Return a choice at random
 function compSelection() {
@@ -23,13 +45,14 @@ function compSelection() {
     compChoice = "scissors";
   }
   console.log("Computer chooses:", compChoice);
+  computerChose.textContent = String("Computer chose: " + compChoice);
   return compChoice;
 }
 
 //plays the game in a loop until 5 points is reached
 function playRound() {
   let computerSelection = compSelection();
-  let playerChoice = playerSelection();
+  // let playerChoice = playerSelection();
   let result = "";
 
   // Compare choices
@@ -47,14 +70,19 @@ function playRound() {
       computerSelection +
       "\nPlayer score: " +
       playerScore +
-      "\nComputer score: ";
+      "\nComputer score: " +
+      computerScore;
     console.log(result);
 
     // "End" the game once a player reaches 5 points
 
     if (playerScore == 5) {
-      result += "You won the game! Reload the page to play again";
+      result += "    You won the game!";
       console.log(result);
+      playAgain.classList.remove("hidden");
+      weapons.classList.add("hidden");
+      playerChose.classList.add("hidden");
+      computerChose.classList.add("hidden");
     }
   } else if (playerChoice == computerSelection) {
     result =
@@ -68,7 +96,7 @@ function playRound() {
   } else {
     computerScore += 1;
     result =
-      "You lose!" +
+      "You lose! " +
       computerSelection +
       " beats " +
       playerChoice +
@@ -79,10 +107,15 @@ function playRound() {
     console.log(result);
 
     if (computerScore == 5) {
-      result += "Computer won the game! Reload the page to play again";
+      result += "     Computer won the game!";
       console.log(result);
+      playAgain.classList.remove("hidden");
+      weapons.classList.add("hidden");
+      playerChose.classList.add("hidden");
+      computerChose.classList.add("hidden");
     }
   }
+  document.getElementById("result").textContent = `${result}`;
 }
 
 // playRound();
